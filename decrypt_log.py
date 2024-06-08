8457ffd543ad3179f95c4883e9aec0243f0ed007
Shivam K <61914107+shivamk2984@users.noreply.github.com>
2024-06-08 20:38:55 +0530

    Update decrypt_log.py

--- decrypt_log.py
+++ decrypt_log.py
@@ -1,11 +1,11 @@
 from cryptography.fernet import Fernet
 
 # Your generated key used in the script
-key = b'key-that-you-have-generated-in-key.key-file'
+key = b'key-that-you-have-generated-in-key.key-file' #paste your key here
 cipher_suite = Fernet(key)
 
 # Encrypted log data 
-encrypted_log = b'log.txt-file-content-paste-here'
+encrypted_log = b'log.txt-file-content-paste-here' # paste log.txt file here
 
 def decrypt_message(encrypted_message):
     return cipher_suite.decrypt(encrypted_message).decode()
